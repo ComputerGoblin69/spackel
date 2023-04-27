5124ee03bce80de86785a40b771e37a38cf729a8
Edvin531 <edvin.bennsten@hotmail.com>
2023-04-27 16:12:34 +0200

    Co-authored-by: ComputerGoblin69 <ComputerGoblin69@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,54 @@ fn real_main() -> Result<(), ()> {
         .ok_or_else(|| eprintln!("Error: no file provided"))?;
     let source_code = std::fs::read_to_string(source_path)
         .map_err(|err| eprintln!("Error: failed to read source file: {err}"))?;
-    print!("{source_code}");
+
+    let mut stack = Vec::<i32>::new();
+
+    for c in source_code.chars() {
+        match c {
+            '0'..='9' => stack.push((c as u32 - '0' as u32) as i32),
+            'p' => println!("{}", stack.pop().unwrap()),
+            '+' => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                stack.push(if a == 9 && b == 10 || b == 9 && a == 10 {
+                    21
+                } else if b == 1 && a == 1 {
+                    1
+                } else {
+                    a + b
+                });
+            }
+            '-' => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                stack.push(b - a);
+            }
+            '*' => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                stack.push(b * a);
+            }
+            '/' => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                stack.push(b / a);
+            }
+            'ß' => stack.push(1945),
+            'x' => {
+                stack.pop().unwrap();
+            }
+            _ => {
+                if !c.is_whitespace() {
+                    panic!("invalid character: {c:?}");
+                }
+            }
+        }
+    }
+
+    if !stack.is_empty() {
+        panic!("there's stuff left on the stack");
+    }
 
     Ok(())
 }
