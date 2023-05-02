a08f8f7c3f0a92d5d41cb433ef93f6fdf67eb021
Johan-Mi <johanmi@protonmail.com>
2023-05-02 10:24:05 +0200

    More error handling

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,68 +1,88 @@
+#![forbid(clippy::unwrap_used)]
+
 use anyhow::{ensure, Context, Result};
 
 pub fn interpret(source_code: &str) -> Result<()> {
-    let mut stack = Vec::<i32>::new();
+    Interpreter { stack: Vec::new() }.interpret(source_code)
+}
 
-    for word in source_code.lines().flat_map(|line| {
-        line.split_once('#')
-            .map_or(line, |(line, _comment)| line)
-            .split_whitespace()
-    }) {
-        match word {
-            "p" => println!("{}", stack.pop().unwrap()),
-            #[allow(clippy::cast_sign_loss)]
-            "c" => print!(
-                "{}",
-                (stack.pop().unwrap() as u32)
-                    .try_into()
-                    .unwrap_or(char::REPLACEMENT_CHARACTER)
-            ),
-            "+" => {
-                let a = stack.pop().unwrap();
-                let b = stack.pop().unwrap();
-                stack.push(match (b, a) {
-                    (9, 10) | (10, 9) => 21,
-                    (1, 1) => 1,
-                    _ => b + a,
-                });
-            }
-            "-" => {
-                let a = stack.pop().unwrap();
-                let b = stack.pop().unwrap();
-                stack.push(b - a);
-            }
-            "*" => {
-                let a = stack.pop().unwrap();
-                let b = stack.pop().unwrap();
-                stack.push(b * a);
-            }
-            "/" => {
-                let a = stack.pop().unwrap();
-                let b = stack.pop().unwrap();
-                stack.push(b / a);
-            }
-            "ß" => stack.push(1945),
-            "x" => {
-                stack.pop().unwrap();
-            }
-            "d" => {
-                let v = *stack.last().unwrap();
-                stack.push(v);
-            }
-            "s" => {
-                let a = stack.len() - 2;
-                let b = stack.len() - 1;
-                stack.swap(a, b);
-            }
-            _ => {
-                stack.push(word.parse().ok().with_context(|| {
-                    format!("unknown instruction: `{word}`")
-                })?);
-            }
-        }
+struct Interpreter {
+    stack: Vec<i32>,
+}
+
+impl Interpreter {
+    fn pop(&mut self) -> Result<i32> {
+        self.stack.pop().context("not enough arguments on stack")
     }
 
-    ensure!(stack.is_empty(), "there's stuff left on the stack");
+    fn push(&mut self, value: i32) {
+        self.stack.push(value);
+    }
 
-    Ok(())
+    fn interpret(&mut self, source_code: &str) -> Result<()> {
+        for word in source_code.lines().flat_map(|line| {
+            line.split_once('#')
+                .map_or(line, |(line, _comment)| line)
+                .split_whitespace()
+        }) {
+            match word {
+                "p" => println!("{}", self.pop()?),
+                #[allow(clippy::cast_sign_loss)]
+                "c" => print!(
+                    "{}",
+                    (self.pop()? as u32)
+                        .try_into()
+                        .unwrap_or(char::REPLACEMENT_CHARACTER)
+                ),
+                "+" => {
+                    let a = self.pop()?;
+                    let b = self.pop()?;
+                    self.push(match (b, a) {
+                        (9, 10) | (10, 9) => 21,
+                        (1, 1) => 1,
+                        _ => b + a,
+                    });
+                }
+                "-" => {
+                    let a = self.pop()?;
+                    let b = self.pop()?;
+                    self.push(b - a);
+                }
+                "*" => {
+                    let a = self.pop()?;
+                    let b = self.pop()?;
+                    self.push(b * a);
+                }
+                "/" => {
+                    let a = self.pop()?;
+                    let b = self.pop()?;
+                    self.push(b / a);
+                }
+                "ß" => self.push(1945),
+                "x" => {
+                    self.pop()?;
+                }
+                "d" => {
+                    let v = self.pop()?;
+                    self.push(v);
+                    self.push(v);
+                }
+                "s" => {
+                    let b = self.pop()?;
+                    let a = self.pop()?;
+                    self.push(b);
+                    self.push(a);
+                }
+                _ => {
+                    self.push(word.parse().ok().with_context(|| {
+                        format!("unknown instruction: `{word}`")
+                    })?);
+                }
+            }
+        }
+
+        ensure!(self.stack.is_empty(), "there's stuff left on the self");
+
+        Ok(())
+    }
 }
