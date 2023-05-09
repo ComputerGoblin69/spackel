40dde0ef76a54de521b3a8b1e7e3638716a5bb5f
Johan-Mi <johanmi@protonmail.com>
2023-05-09 18:32:26 +0200

    Delete `iter.rs`

--- src/iter.rs
+++
@@ -1,6 +0,0 @@
-pub fn batching_map<I: Iterator, O>(
-    mut it: I,
-    mut f: impl FnMut(&mut I, I::Item) -> O,
-) -> impl Iterator<Item = O> {
-    std::iter::from_fn(move || it.next().map(|t| f(&mut it, t)))
-}
